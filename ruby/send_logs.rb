5e1038fefc8c153139e9dad31278739817329d6e
Chris Smith <cleesmith2006@gmail.com>
2014-04-16 13:56:00 -0400

    add snort rulebase

--- ruby/send_logs.rb
+++ ruby/send_logs.rb
@@ -1,4 +1,9 @@
 #!/usr/bin/env ruby
+# usage:
+#   ruby send_logs.rb
+#   ... will send 1 batch of the logs (see client.puts below)
+#   ruby send_logs.rb 50
+#   ... will send 50 batches of the logs (see client.puts below)
 require 'socket'
 require 'timeout'
 
@@ -12,19 +17,20 @@ s = Time.now
 client = TCPSocket.new('127.0.0.1', 514)
 total = 0
 begin
-  # send 500 * 20 = 10,000
-  # send 50 * 20 = 1,000
   do_it.times do |x|
-    # cls: does not work:
-    client.puts "Apr 15 18:50:00 clstesting test: hello 55"
+    # client.puts "Apr 16 14:25:00 testing_server test: hello 55 goodbye"
     # client.puts "Apr 9 15:22:28 digitalocean sshd[27780]: Accepted password for osprotect from 204.111.71.34 port 51364 ssh2"
     # client.puts "Dec 23 12:11:43 digitalocean postfix/smtpd[31499]: connect from unknown[95.75.93.154] via ruby"
     # client.puts "Dec 23 12:11:43 digitalocean postfix/smtpd[31499]: connect from unknown[95.75.93.154]"
     # client.puts "Dec 23 14:42:56 digitalocean named[16000]: client 199.48.164.7#64817: query (cache) 'amsterdamboothuren.com/MX/IN' denied"
     # client.puts "Dec 23 14:30:01 digitalocean CRON[619]: (www-data) CMD (php /usr/share/cacti/site/poller.php >/dev/null 2>/var/log/cacti/poller-error.log)"
     # client.puts "Dec 22 18:28:06 digitalocean rsyslogd: [origin software='rsyslogd' swVersion='4.2.0' x-pid='2253' x-info='http://www.rsyslog.com'] rsyslogd was HUPed, type 'lightweight'."
-
     # client.puts "Service_Control_Manager: 7035: NT AUTHORITYSYSTEM: *cls* The COH_Mon service was successfully sent a start control."
+    # client.puts "SceCli 1202: Security policies were propagated with warning. 0x4b8 : An extended error has occurred. For best results in resolving this event, log on with a non-administrative account and search http://support.microsoft.com for \"Troubleshooting Event 1202's\"."
+    client.puts "Apr 16 14:25:00 x.x.x.x snort [1:2001415:5] ICMP Destination Unreachable Communication Administratively Prohibited [Classification: Misc activity] [Priority: 3] {ICMP} 192.168.1.1 -> 10.0.0.0"
+    client.puts "Jan 18 19:19:51 server1 suricata[88343]: [1:2001219:18] ET SCAN Potential SSH Scan [Classification: Attempted Information Leak] [Priority: 2] {TCP} 1.2.3.4:33835 -> 4.3.2.1:22"
+    client.puts "Feb 18 19:19:45 server1 suricata[88343]: [1:2016292:3] ET TROJAN RevProxy ClickFraud - hello [Classification: A Network Trojan was detected] [Priority: 1] {TCP} 1.2.3.4:30180 -> 4.3.2.1:443"
+    # client.puts "Service_Control_Manager: 7035: NT AUTHORITYSYSTEM: The COH_Mon service was successfully sent a start control."
     # client.puts "SceCli 1202: Security policies were propagated with warning. 0x4b8 : An extended error has occurred. For best results in resolving this event, log on with a non-administrative account and search http://support.microsoft.com for \"Troubleshooting Event 1202's\"."
     # client.puts "snort [1:2001415:5] ICMP Destination Unreachable Communication Administratively Prohibited [Classification: Misc activity] [Priority: 3] {ICMP} 192.168.1.1 -> 10.0.0.0"
     # client.puts "url 192.168.1.1,10.0.0.0,GET,ajax.googleapis.com,/ajax/libs/jqueryui/1.7.2/jquery-ui.min.js,http://slickdeals.net/,Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; .NET CLR 2.0.50727; .NET CLR 3.0.4506.2152; .NET CLR 3.5.30729)|,com,googleapis.com,ajax.googleapis.com|200|46142|8583"
